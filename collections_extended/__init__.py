483afd6b071a998c2f8c4e22f619b8af3d320701
Mike Lenzen <m.lenzen@gmail.com>
2014-05-24 16:32:18 -0500

    Removed ABCs and no longer "extend" collections.

--- collections_extended/__init__.py
+++ collections_extended/__init__.py
@@ -1,15 +1,11 @@
 '''collections_extended contains a few extra basic data structures'''
 
-__version__ = '0.1.2'
-
-from abc import ABCMeta, abstractmethod
-from collections import *
-import collections
+__version__ = '0.1.3'
 
 from .bags import bag, frozenbag
 from .setlists import setlist, frozensetlist
 
-__all__ = ['collection', 'Collection', 'MutableCollection', 'MutableSequence', 'Set', 'setlist', 'frozensetlist', 'bag', 'frozenbag'] + collections.__all__
+__all__ = ['collection', 'setlist', 'frozensetlist', 'bag', 'frozenbag']
 
 def collection(it=(), mutable=True, ordered=False, unique=False):
 	""" Return a Collection with the specified properties. 
@@ -53,84 +49,4 @@ def collection(it=(), mutable=True, ordered=False, unique=False):
 				return bag(it)
 			else:
 				return frozenbag(it)
-
-#####################################################################
-## ABCs
-#####################################################################
-
-class Collection(Sized, Iterable, Container):
-	""" An ABC for all collections.
-	>>> isinstance(list(), Collection)
-	True
-	>>> isinstance(set(), Collection)
-	True
-	>>> isinstance(bag(), Collection)
-	True
-	>>> isinstance(setlist(), Collection)
-	True
-	"""
-	@classmethod
-	def _from_iterable(cls, it):
-		""" Construct an instance of the class from any iterable input.
-
-		Must override this method if the class constructor signature
-		does not accept an iterable for an input.
-		"""
-		return cls(it)
-
-	def count(self, elem):
-		count = 0
-		for e in self:
-			if e == elem:
-				count += 1
-		return count
-
-Collection.register(Sequence)
-Collection.register(Set)
-Collection.register(bag)
-Collection.register(frozenbag)
-
-class MutableCollection(Collection):
-	""" A metaclass for all MutableCollection objects.
-	
-	>>> isinstance(list(), MutableCollection)
-	True
-	>>> isinstance(set(), MutableCollection)
-	True
-	>>> isinstance(bag(), MutableCollection)
-	True
-	>>> isinstance(setlist(), MutableCollection)
-	True
-	"""
-	@abstractmethod
-	def add(self, value):
-		raise ValueError
-
-	@abstractmethod
-	def remove(self, value):
-		raise ValueError
-
-	@abstractmethod
-	def pop(self):
-		raise KeyError
-
-MutableCollection.register(MutableSequence)
-MutableCollection.register(MutableSet)
-MutableCollection.register(bag)
-
-#####################################################################
-## Extending built-in classes
-#####################################################################
-
-class Set(Set):
-	""" Extended Set to efficiently count elements. """
-
-	def count(self, elem):
-		return elem in self
-
-class MutableSequence(MutableSequence):
-	""" Extended MutableSequence to fit Collection ABC """
-
-	def add(self, value):
-		return self.append(value)
 
