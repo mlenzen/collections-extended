920c299e995919579f69433f6f2254950996f235
Mike Lenzen <m.lenzen@gmail.com>
2014-12-21 23:19:20 -0600

    Added travisci file and added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
 from os.path import dirname, join
 
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
 import collections_extended
+
+class PyTest(TestCommand):
+	def finalize_options(self):
+		TestCommand.finalize_options(self)
+		self.test_args = []
+		self.test_suite = True
+
+	def run_tests(self):
+		import pytest
+		errcode = pytest.main(self.test_args)
+		sys.exit(errcode)
 
 setup(
 		name='data-structures',
@@ -25,4 +38,5 @@ setup(
 			'Topic :: Software Development :: Libraries :: Python Modules',
 			],
 		long_description = open(join(dirname(__file__), 'README.rst')).read(),
+		tests_require=['pytest'],
 		)
