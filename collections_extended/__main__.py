16a7dfa7d91e43287d420af0ca855a8722fd9805
Mike Lenzen <m.lenzen@gmail.com>
2014-12-21 23:15:04 -0600

    Removed __main__ script than ran doctests.

--- collections_extended/__main__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import doctest
-
-import collections_extended
-
-doctest.testmod(collections_extended)
-doctest.testmod(collections_extended.bags)
-doctest.testmod(collections_extended.setlists)
-
