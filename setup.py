f45bafd363695e127afd3115b273936f9e250c53
Mike Lenzen <m.lenzen@gmail.com>
2015-01-19 13:52:54 -0600

    Updates to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from os.path import dirname, join
+import sys
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-import collections_extended
 
 class PyTest(TestCommand):
 	def finalize_options(self):
@@ -19,24 +19,32 @@ class PyTest(TestCommand):
 setup(
 		name='data-structures',
 		packages=['collections_extended'],
-		version = collections_extended.__version__,
-		description = 'Extra Python Data Structures - bags (multisets) and setlists (ordered sets)',
-		author = 'Michael Lenzen',
-		author_email = 'm.lenzen@gmail.com',
+		version='0.1.4',
+		description=(
+			'Extra Python Data Structures - bags (multisets) and setlists (ordered'
+			' sets)'
+			),
+		author='Michael Lenzen',
+		author_email='m.lenzen@gmail.com',
 		license='Apache License, Version 2.0',
-		url = 'https://github.com/mlenzen/python-data-structures',
-		keywords = ['collections', 'bag', 'multiset', 'setlist', 'ordered set', 'unique list'],
-		classifiers = [
+		url='https://github.com/mlenzen/python-data-structures',
+		keywords=['collections', 'bag', 'multiset', 'setlist', 'ordered set', 'unique list'],
+		classifiers=[
 			'Development Status :: 4 - Beta',
 			'Intended Audience :: Developers',
 			'License :: OSI Approved :: Apache Software License',
 			'Operating System :: OS Independent',
 			'Programming Language :: Python',
 			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.2',
+			'Programming Language :: Python :: 3.3',
+			'Programming Language :: Python :: 3.4',
 			'Topic :: Software Development',
 			'Topic :: Software Development :: Libraries',
 			'Topic :: Software Development :: Libraries :: Python Modules',
 			],
 		long_description = open(join(dirname(__file__), 'README.rst')).read(),
+		install_requires=['setuptools'],
 		tests_require=['pytest'],
+		package_data={'': ['README.rst', 'LICENSE']},
 		)
