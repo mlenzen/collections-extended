d7c20e367c8686fecd35a956b1bf78ea3a0f9d39
Mike Lenzen <m.lenzen@gmail.com>
2015-01-31 14:07:38 -0600

    Add tests for bijection.

---
+++ tests/test_bijection.py
@@ -0,0 +1,24 @@
+
+import pytest
+
+from collections_extended.bijection import bijection
+
+
+def test_bijection():
+	b = bijection()
+	assert len(b) == 0
+	b['a'] = 1
+	assert len(b) == 1
+	assert b['a'] == 1
+	assert b.inverse[1] == 'a'
+	assert 'a' in b
+	assert 1 not in b
+	assert 1 in b.inverse
+	with pytest.raises(KeyError):
+		del b['f']
+	assert b == bijection(a=1)
+	assert b.inverse.inverse is b
+	assert b == b.copy()
+	del b['a']
+	assert b == bijection()
+	assert bijection(a=1, b=2, c=3) == bijection({'a':1, 'b': 2, 'c': 3})
