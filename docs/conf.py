8368894394ce487b62b37865ae9ee8f92022958a
Michael Lenzen <m.lenzen@gmail.com>
2016-01-13 00:28:49 -0600

    Add Sphinx theme for local build.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-	#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # complexity documentation build configuration file, created by
@@ -32,6 +32,14 @@ project_root = os.path.dirname(cwd)
 sys.path.insert(0, project_root)
 
 import collections_extended
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration ---------------------------------------------
 
@@ -111,7 +119,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
