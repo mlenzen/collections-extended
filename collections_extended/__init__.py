cf35ddff5ec5d0e934695eaada8a642c9ed4206b
Michael Lenzen <m.lenzen@gmail.com>
2018-04-14 09:35:27 -0500

    Bump version: 1.0.0 → 1.0.1

--- collections_extended/__init__.py
+++ collections_extended/__init__.py
@@ -5,7 +5,7 @@ from .setlists import setlist, frozensetlist
 from .bijection import bijection
 from .range_map import RangeMap, MappedRange
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __all__ = (
 	'collection',
