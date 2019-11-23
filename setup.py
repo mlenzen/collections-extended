33576e16e1e423791177eced9f9f68d95a28f046
Michael Lenzen <m.lenzen@gmail.com>
2019-11-23 02:57:47 -0600

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 setup(
 	name='collections-extended',
 	packages=['collections_extended'],
-	version='1.0.2',
+	version='1.0.3',
 	description=(
 		'Extra Python Collections - bags (multisets) and setlists (ordered'
 		' sets)'
